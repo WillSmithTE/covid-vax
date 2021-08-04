c97430330899c9906bcd012f3b1fbdc62d6ae25a
Will Smith <willsmithte@gmail.com>
2021-08-05 08:19:31 +1000

    use coloured dots and trendline

--- src/AgeToVaccinationRateGraph.js
+++ src/AgeToVaccinationRateGraph.js
@@ -22,22 +22,27 @@ export const AgeToVaccinationRateGraph = () => {
         width={'800px'}
         height={'500px'}
         chartArea={{ width: "100%", height: "100%" }}
-        chartType="ScatterChart"
+        chartType="LineChart"
         loader={<div>Loading Chart</div>}
-        data={[
-            // ['Age', 'Vaccinated'],
-            // ...manipulatedData,
-            ['x', ...manipulatedData.placeNames],
-            ...manipulatedData.data,
-        ]}
+        data={
+            [
+                ['x', 'Vaccinated', ...manipulatedData.placeNames],
+                ...manipulatedData.data
+            ]
+        }
         options={{
             title: 'Age vs Vaccination Rate',
             hAxis: { title: 'Median age' },
             vAxis: { title: '% at least 1 dose' },
             // legend: 'none',
             trendlines: {
-                0: { type: 'linear', showR2: true, visibleInLegend: true }
+                0: { type: 'linear', showR2: true, visibleInLegend: true, lineWidth: 3, pointSize: 0, }
             },
+            lineWidth: 0,
+            pointSize: 5,
+            series: {
+                0: { pointSize: 0 },
+              },          
         }}
         rootProps={{ 'data-testid': '1' }}
     /> : <h1>loading...</h1>
@@ -47,9 +52,10 @@ function manipulate({ Data }) {
     const onlySydney = Data.filter(({ Area }) => Area.Name4.includes('Sydney'))
     const data = onlySydney
         .map(({ Area, CovidVaccine }, index) => {
-            const row = new Array(onlySydney.length + 1).fill(null);
+            const row = new Array(onlySydney.length + 2).fill(null);
             row[0] = Area.CensusStats.Age.Median;
-            row[index + 1] = CovidVaccine.Num1Dose;
+            row[1] = CovidVaccine.Num1Dose
+            row[index + 2] = CovidVaccine.Num1Dose;
             return row;
         });
 
